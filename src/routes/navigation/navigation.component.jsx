1eb06ad7a96ed29104d98332ffd696cde01bb9d3
Guilherme <gcaramori16@gmail.com>
2022-06-01 23:04:31 -0300

    finished routing w/ react-router

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import './navigation.scss';
+
+const Navigation = () => {
+    return(
+        <Fragment>
+            <div className="navigation">
+                <Link className="nav-link" to='/'>
+                    <CrwnLogo className='logo'></CrwnLogo>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation
