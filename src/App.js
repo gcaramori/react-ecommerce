de0a944472e7461bf869d78c1cf12c55961fc48c
Guilherme <guilherme@sunsetsolutions.com.br>
2022-06-08 12:46:24 -0300

    initing shop component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
-import logo from './logo.svg';
+import Shop from './routes/shop/shop.component';
 import './App.css';
-
-const Shop = () => {
-  return <h1>I am the Shop Page!</h1>
-}
 
 const App = () => {
   return(
